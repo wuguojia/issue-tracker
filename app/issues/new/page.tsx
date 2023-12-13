74859fb7fe3b42a03716588a0d9f2751efe86252
wuguojia <956957153@qq.com>
2023-12-13 20:04:04 +0800

    [CHG]: add react-simlemde-editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,19 @@
-'use client'
-import React from 'react';
-import {Button, TextArea, TextField} from "@radix-ui/themes";
+"use client";
+import React from "react";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
-    return (
-        <div className="max-w-xl space-y-3">
-            <TextField.Root>
-                <TextField.Input placeholder="Title" />
-            </TextField.Root>
-            <TextArea placeholder="Description" />
-            <Button>Submit Now</Button>
-        </div>
-    );
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <SimpleMDE placeholder="Description" />
+      <Button>Submit Now</Button>
+    </div>
+  );
 };
 
 export default NewIssuePage;
