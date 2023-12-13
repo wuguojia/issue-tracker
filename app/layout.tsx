f915b8b688d15628f2582637dfaefb5748395078
wuguojia <956957153@qq.com>
2023-12-13 19:54:29 +0800

    [CHG]: customize radix ui - fix font important

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className} ${inter.variable}}`}>
+    <html lang="en" className={inter.variable}>
       <body>
         <Theme appearance="light" accentColor="violet">
           <NavBar />
